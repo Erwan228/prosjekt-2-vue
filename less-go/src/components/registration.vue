7df4376edc7dc66667e8bcff420b5787542cd1f9
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-06-14 10:52:45 +0200

    Update registration.vue

--- less-go/src/components/registration.vue
+++ less-go/src/components/registration.vue
@@ -1,5 +1,7 @@
 <template>
-
+ <form>
+    <label>Name:</label>
+ </form>
 </template>
 
 <script>
@@ -7,5 +9,21 @@
 </script>
 
 <style>
-
+form {
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+}
+label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+}
 </style>
