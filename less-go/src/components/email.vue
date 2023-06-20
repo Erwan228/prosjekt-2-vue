144f82ae493e84c8e6aa74aea6b470894a9dcbe7
Erwan Foxtail <84564131+Erwan228@users.noreply.github.com>
2023-06-20 14:36:09 +0200

    Update email.vue

--- less-go/src/components/email.vue
+++ less-go/src/components/email.vue
@@ -3,7 +3,7 @@
     Subject:<br>
     <input type="text" name="subject" value="Fighter"><br>
     Message:<br>
-    <textarea name="body" rows="5" cols="30" >{{ infoenTilEmail }}</textarea><br>
+    <textarea name="body" rows="5" cols="30" spellcheck="false">{{ infoenTilEmail }}</textarea><br>
     <input type="submit" value="Send">
     </form>
 </template>
@@ -15,6 +15,12 @@ export default{
 
 </script>
 
-<style>
-
+<style scoped>
+form {
+    max-width: 420px;
+    margin: 30px auto;
+    
+    padding: 40px;
+    border-radius: 10px;
+}
 </style>
